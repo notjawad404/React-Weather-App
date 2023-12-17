de5cc2122fbd9653456e5ef593e851d7c90f373f
notjawad404 <jawadali000123@gmail.com>
2023-12-17 20:58:39 +0500

    Removed unnecessary line of code

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -13,17 +13,14 @@ export default function Weather() {
 
   let ApiKey = '2cb6b07f4fc0a3d186c0fb2034a443f4';
 
-  // const [wicon, setWicon] = useState(cloud_icon);
-  // https://api.openweathermap.org/data/2.5/weather?q=Islamabad&units=Metric&appid=2cb6b07f4fc0a3d186c0fb2034a443f4
+  const search = async () => {
+    const element = document.getElementsByClassName('cityInput');
+    if (element[0].value === '') {
+      alert('Please enter city name');
+      return 0;
+    }
 
-  const search = async () => { 
-      const element = document.getElementsByClassName('cityInput');
-      if(element[0].value === ''){
-        alert('Please enter city name');
-        return 0;
-      }
-
-   let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${ApiKey}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${ApiKey}`;
 
 
     let response = await fetch(url);
@@ -53,53 +50,44 @@ export default function Weather() {
     min_temp[0].innerHTML = data.main.temp_min + '°C';
     max_temp[0].innerHTML = data.main.temp_max + '°C';
 
-    if(data.weather[0].main === 'Clouds'){
+    if (data.weather[0].main === 'Clouds') {
       icon[0].src = cloud_icon;
     }
-    else if(data.weather[0].main === 'Clear'){
+    else if (data.weather[0].main === 'Clear') {
       icon[0].src = clear_icon;
     }
-    else if(data.weather[0].main === 'Rain'){
+    else if (data.weather[0].main === 'Rain') {
       icon[0].src = rain_icon;
     }
-    else if(data.weather[0].main === 'Snow'){
+    else if (data.weather[0].main === 'Snow') {
       icon[0].src = snow_icon;
     }
-    else if(data.weather[0].main === 'Drizzle'){
+    else if (data.weather[0].main === 'Drizzle') {
       icon[0].src = drizzle_icon;
     }
-    else{
+    else {
       icon[0].src = cloud_icon;
     }
-
-
-
-
-
-
-    console.log(data);
-
   }
   return (
     <div className='h-screen overflow-y-scroll bg-img1'>
       <div className='  w-3/4 m-auto mt-6 overflow-y-auto h-95' >
         <div className='flex justify-center pt-4 gap-4'>
           <input type='text' className='cityInput flex w-96 h-8 border-none outline-none rounded-3xl pl-10 text-gray-500 font-normal' placeholder='Search for places' />
-          <div className='flex justify-center items-center bg-white w-8 h-8 bg-black-50 rounded-3xl cursor-pointer' onClick={()=>{search()}}>
+          <div className='flex justify-center items-center bg-white w-8 h-8 bg-black-50 rounded-3xl cursor-pointer' onClick={() => { search() }}>
             <img src={search_icon} alt='search Icon' />
           </div>
 
         </div>
 
-
         <div className='mt-7 flex justify-center'>
-          <img src={cloud_icon} alt='weather Image' className='weather-image  w-20 h-20'/>
+          <img src={cloud_icon} alt='weather Image' className='weather-image  w-20 h-20' />
         </div>
 
         <div className='weather-status mt-7 flex justify-center text-white text-lg font-normal'>
           status
         </div>
-        
+
         <div className='weather-temp mt-7 flex justify-center text-white  font-normal text-3xl'>
           -°C
         </div>
@@ -107,10 +95,10 @@ export default function Weather() {
           City
         </div>
         <div className='flex justify-center pt-4'>
-        <div className='m-auto flex items-start gap-3 text-white'>
+          <div className='m-auto flex items-start gap-3 text-white'>
             <div className='text-3xl font-normal'>
               <div className='min-temp'>
-              -°C
+                -°C
               </div>
               <div className='text-lg font-normal'>
                 Min Temp
@@ -120,7 +108,7 @@ export default function Weather() {
           <div className='m-auto flex items-start gap-3 text-white'>
             <div className='text-3xl font-normal'>
               <div className='max-temp'>
-              -°C
+                -°C
               </div>
               <div className='text-lg font-normal'>
                 Max Temp
@@ -133,7 +121,7 @@ export default function Weather() {
             <img src={humidity_icon} alt='icon' className='icon mt-3' />
             <div className='text-3xl font-normal'>
               <div className='humidity-percentage'>
-              -%
+                -%
               </div>
               <div className='text-lg font-normal'>
                 Humidity
@@ -144,21 +132,21 @@ export default function Weather() {
             <img src={wind_icon} alt='icon' className='icon mt-3 ' />
             <div className='text-3xl font-normal'>
               <div className='wind-speed'>
-              -Km/h
+                -Km/h
               </div>
               <div className='text-lg font-normal'>
                 wind Speed
               </div>
             </div>
           </div>
-          
-          </div>
-        </div>
-        <div className='mt-12 text-white flex justify-center'>
-        Copyright © 2023 . All rights reserved.
 
         </div>
       </div>
-    
+      <div className='mt-12 text-white flex justify-center'>
+        Copyright © 2023 . All rights reserved.
+
+      </div>
+    </div>
+
   )
 }
